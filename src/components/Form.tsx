0cf3d00dbada877f539b9b1c8c34a0c83b70e3d6
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-05-18 09:10:02 -0500

    added error behaviour

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,27 +1,51 @@
 import React from "react";
 import { Formik } from "formik";
 import { TextInput } from "./TextInput";
+import * as Yup from "yup";
 
 export const Form = () => {
-    return (
-        <Formik
-            initialValues={{ firstName: "", lastName: "", email: "", password: "" }}
-            onSubmit={(values) => {
-                console.log(values);
-            }}
-        >
-            {() => (
-                <form action="" className="form__container">
-                    <TextInput name="firstName" type="text" placeholder="First Name" />
-                    <TextInput name="lastName" type="text" placeholder="Last Name" />
-                    <TextInput name="email" type="email" placeholder="Email" />
-                    <TextInput name="firstName" type="text" placeholder="First Name" />
-                    <button className="form__button">CLAIM YOUR FREE TRIAL</button>
-                    <span className="form__span">By clicking the button, you are agreeing to our <strong className="form__span-strong">
-                        Terms and Services
-                    </strong>  </span>
-                </form>
-            )}
-        </Formik>
-    );
+  const validate = Yup.object({
+    firstName: Yup.string()
+      .max(15, "Must be 15 characters")
+      .required("First Name cannot be empty"),
+
+    lastName: Yup.string()
+      .max(15, "Must be 15 characters")
+      .required("Last Name cannot be empty"),
+
+    email: Yup.string()
+      .email("Looks like this is not an email")
+      .max(24, "Must be 25 characters")
+      .required("Email cannot be empty"),
+
+    password: Yup.string()
+      .min(6, "Must be minimun 6 characters")
+      .required("Password cannot be empty"),
+  });
+
+  return (
+    <Formik
+      initialValues={{ firstName: "", lastName: "", email: "", password: "" }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+      validationSchema={validate}
+    >
+      {() => (
+        <form action="" className="form__container">
+          <TextInput name="firstName" type="text" placeholder="First Name" />
+          <TextInput name="lastName" type="text" placeholder="Last Name" />
+          <TextInput name="email" type="email" placeholder="Email" />
+          <TextInput name="firstName" type="text" placeholder="First Name" />
+          <button type="submit" className="form__button">
+            CLAIM YOUR FREE TRIAL
+          </button>
+          <span className="form__span">
+            By clicking the button, you are agreeing to our{" "}
+            <strong className="form__span-strong">Terms and Services</strong>{" "}
+          </span>
+        </form>
+      )}
+    </Formik>
+  );
 };
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,11 +1,23 @@
-import { FieldHookConfig, useField } from "formik";
 import React from "react";
+import { FieldHookConfig, useField, ErrorMessage } from "formik";
+import iconError from '../images/icon-error.svg'
 
-export const TextInput = (props: React.InputHTMLAttributes<HTMLInputElement> & FieldHookConfig<string>) => {
+export const TextInput = (props) => {
   const [field, meta] = useField(props);
 
   return (
-   
-      <input {...props} className="input__text" />
+    <div className="input__form-container">
+      <input
+        {...field}
+        {...props}
+        className={`input__text ${meta.touched && meta.error && "is-invalid"}`}
+      />
+      <img className={`incon-error ${meta.touched && meta.error && "display-span"}`} src={iconError} alt="" />
+      <ErrorMessage
+        component="span"
+        name={field.name}
+        className='input__span-error'
+      />
+    </div>
   );
 };
